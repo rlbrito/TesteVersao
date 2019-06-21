f237cf83abf9ed0827cfb1dc940b17f61d97d60b
ricardo <rlbrito@yahoo.com.br>
2019-06-21 10:58:50 -0300

    Teste Master

---
+++ ConsoleApplication/ConsoleApplication/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string xx = "Teste de projeto";
+            Console.WriteLine("Inicio");
+            Console.WriteLine(xx);
+        }
+    }
+}
